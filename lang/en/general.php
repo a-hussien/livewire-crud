f800d43e7fd0d2a4c69fecccdeeb0fae0743b505
Ahmed Hussein <a.hussien87@gmail.com>
2024-02-20 15:38:39 +0200

    Updated languages files

--- lang/en/general.php
+++ lang/en/general.php
@@ -14,4 +14,10 @@ return [
     'books' => 'Books',
     'profile'=> 'Profile',
     'logout' => 'Logout',
+    'remember' => 'Remember me',
+    'forgot_password' => 'Forgot your password?',
+    'login' => 'Login',
+    'register' => 'Register',
+    'forgot_password_phrase' => 'Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.',
+    'send_password_reset_link' => 'Email Password Reset Link',
 ];
--- lang/en/validation.php
+++ lang/en/validation.php
@@ -186,6 +186,42 @@ return [
     |
     */
 
-    'attributes' => [],
+    'attributes' => [
+        'name'                  => 'Name',
+        'username'              => 'Username',
+        'email'                 => 'Email',
+        'first_name'            => 'First Name',
+        'last_name'             => 'Last Name',
+        'password'              => 'Password',
+        'password_confirmation' => 'Password Confirmation',
+        'city'                  => 'City',
+        'country'               => 'Country',
+        'address'               => 'Address',
+        'phone'                 => 'Phone',
+        'mobile'                => 'Mobile',
+        'age'                   => 'Age',
+        'sex'                   => 'Sex',
+        'gender'                => 'Gender',
+        'day'                   => 'Day',
+        'month'                 => 'Month',
+        'year'                  => 'Year',
+        'hour'                  => 'Hour',
+        'minute'                => 'Minute',
+        'second'                => 'Second',
+        'content'               => 'Content',
+        'description'           => 'Description',
+        'excerpt'               => 'Excerpt',
+        'date'                  => 'Date',
+        'time'                  => 'Time',
+        'available'             => 'Available',
+        'size'                  => 'Size',
+        'price'                 => 'Price',
+        'desc'                  => 'Description',
+        'title'                 => 'Title',
+        'q'                     => 'Search',
+        'link'                  => 'Link',
+        'slug'                  => 'Slug',
+
+    ],
 
 ];
